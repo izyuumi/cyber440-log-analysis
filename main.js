438f3a1600702d294f9d31e49e693b78da3c5802
Yumi Izumi <me@yumiizumi.com>
2024-11-10 19:27:02 -0500

    volume mounting

--- main.js
+++ main.js
@@ -9,8 +9,32 @@ fs.readFile("Elk Logs", "utf8", (err, data) => {
   }
   const logs = JSON.parse(data);
 
-  for (const log of logs.aggregations[2].buckets) {
-    console.log(log.key_as_string.substring(5, 16));
-    // console.log(log.doc_count);
+  let hits = logs.hits.hits;
+  let start = 0;
+  let success = 0;
+  let failed = 0;
+
+  for (let i = 0; i < hits.length; i++) {
+    let message = hits[i]._source.message;
+    if (message.includes("volume")) {
+      let timestamp = hits[i]._source["@timestamp"];
+      if (message.includes("started")) {
+        start++;
+        // console.log(`${timestamp},started`);
+      }
+      if (message.includes("successfully")) {
+        success++;
+        // console.log(`${timestamp},success`);
+      }
+      if (message.includes("failed")) {
+        failed++;
+        console.log(`${timestamp},failed`);
+        console.log(`${message}`);
+      }
+    }
   }
+
+  console.log(`Total Volume Started: ${start}`);
+  console.log(`Total Volume Success: ${success}`);
+  console.log(`Total Volume Failed: ${failed}`);
 });
